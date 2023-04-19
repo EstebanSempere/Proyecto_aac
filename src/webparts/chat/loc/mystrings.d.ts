14f7bd7c4a52c6a090b0fe3bf729e8838184eb99
EstebanSempere <131277219+EstebanSempere@users.noreply.github.com>
2023-04-19 16:48:19 +0200

    Sharepoint

---
+++ src/webparts/chat/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IChatWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ChatWebPartStrings' {
+  const strings: IChatWebPartStrings;
+  export = strings;
+}
